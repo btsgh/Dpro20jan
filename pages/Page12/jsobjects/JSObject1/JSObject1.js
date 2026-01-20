2a06b6054cdb612fd26ef7c6f3f9cc962d2c70c2
Aparna Ramachandran <101863839+btsgh@users.noreply.github.com>
2026-01-20 11:58:10 +0530

    Update JSObject1.js

--- pages/Page12/jsobjects/JSObject1/JSObject1.js
+++ pages/Page12/jsobjects/JSObject1/JSObject1.js
@@ -1,7 +1,7 @@
 export default {
 	welcome: `Ahoy there! Welcome to our group! How are you doing? While you explore, here's a link to the new member paperwork:
 `,
-	renewal: `A friendly reminder that your membership is about to expire. Please renew your subscriptions at:
+	renewal: `Folks, this is a friendly reminder that your membership is about to expire. Please renew your subscriptions at:
 `,
 	thankyou: `Thank you for supporting our group! Your membership has been renewed.
 `,
